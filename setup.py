8ed58cc860e9b7cebe17d5334cef29d2bbee9b3e
Henrik Palmlund Wahlgren <henrik@pwit.se>
2021-08-26 11:48:18 +0200

    Bump version: 0.0.7 → 0.0.8

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ with open('HISTORY.md') as history_file:
 
 setup(
     name=NAME,
-    version='0.0.7',
+    version='0.0.8',
     python_requires=REQUIRES_PYTHON,
     description=DESCRIPTION,
     long_description=readme + "\n\n" + history,
